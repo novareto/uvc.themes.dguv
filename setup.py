89c3ffcf9689a20de0d79df155b4756a3a8f0cb2
Souheil CHELFOUH <trollfot@gmail.com>
2014-03-14 11:22:58 +0100

    Initial commit

---
+++ setup.py
@@ -0,0 +1,45 @@
+1# -*- coding: utf-8 -*-
+
+from os.path import join
+from setuptools import setup, find_packages
+
+name = 'uvc.themes.dguv'
+version = '0.1-dev'
+readme = open('README.txt').read()
+history = open(join('docs', 'HISTORY.txt')).read()
+
+install_requires = [
+    'zope.interface',
+    ]
+
+tests_require = [
+    ]
+
+setup(name=name,
+      version=version,
+      description=("UVC Theme for the DGUV projects"),
+      long_description=readme + '\n\n' + history,
+      keywords='',
+      author='Novareto',
+      author_email='',
+      url='',
+      license='BSD',
+      package_dir={'': 'src'},
+      packages=find_packages('src', exclude=['ez_setup']),
+      namespace_packages=['uvc', 'uvc.themes'],
+      include_package_data=True,
+      zip_safe=False,
+      tests_require=tests_require,
+      install_requires=install_requires,
+      extras_require={'test': tests_require},
+      classifiers=[
+          'Environment :: Web Environment',
+          'Intended Audience :: Other Audience',
+          'License :: OSI Approved :: GNU General Public License (GPL)',
+          'Operating System :: OS Independent',
+          'Programming Language :: Python',
+          ],
+      entry_points="""
+      # -*- Entry points: -*-
+      """,
+      )
---
+++ src/uvc/themes/dguv/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from .menus import (
+    IGlobalMenu,
+    IPersonalPreferences,
+    IFooterMenu,
+    IDocumentActions,
+    IExtraViews,
+    IPersonalMenu,
+    IContextualActionsMenu,
+    ISubMenu,
+    )
+
+from .managers import (
+    
+IPageTop
+ITabs
+IAboveContent
+IBelowContent
+IHeaders
+IToolbar
+IFooter
+IExtraInfo
