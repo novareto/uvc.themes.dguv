990e6c67549b40e36d08418e0e2c2b53c419f916
goschtl <ck@novareto.de>
2014-03-31 14:54:54 +0200

    changes

---
+++ src/uvc/themes/dguv/forms.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+import uvclight
+
+from . import IDGUVRequest
+from cromlech.browser import ITemplate
+from dolmen.forms.base import interfaces
+from dolmen.forms.base.interfaces import IForm
+from dolmen.forms.base.widgets import ActionWidget
+from grokcore.component import adapter, implementer, adapts
+
+
+class ActionWidget(ActionWidget):
+    adapts(
+        interfaces.IAction,
+        interfaces.IFieldExtractionValueSetting,
+        IDGUVRequest)
+
+    def htmlClass(self):
+        return "action btn btn-default"
+
+
+@adapter(IForm, IDGUVRequest)
+@implementer(ITemplate)
+def form_template(context, request):
+    """default template for the menu"""
+    return uvclight.get_template('form.cpt', __file__)
